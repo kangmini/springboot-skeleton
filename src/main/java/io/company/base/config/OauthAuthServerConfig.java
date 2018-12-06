861ebb6c7f3b51993cae5fc3a041dab9c6eab3bc
kangmini <kimkm212@gmail.com>
2018-12-06 12:10:20 +0900

    임시 commit

--- src/main/java/io/company/base/config/OauthAuthServerConfig.java
+++ src/main/java/io/company/base/config/OauthAuthServerConfig.java
@@ -2,11 +2,8 @@ package io.company.base.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
